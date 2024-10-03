c783fd69bc8a62a9f357220db49c2870eabb94c8
amin <aminarshadinia@gmail.com>
2024-10-04 00:49:27 +0800

    feat: create home component

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+  <div>
+    <div class="knowledge-base">
+      <div v-if="loading" class="loading">Loading categories...</div>
+      <div v-else-if="error" class="error"></div>
+      <div v-else class="categories">
+        <router-link 
+
+          class="category"
+        >
+           <icon name="link"></icon>
+          <p class="category-title"></p>
+          <p class="category-article">articles</p>
+          <p class="category-update">Last update</p>
+        </router-link>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
