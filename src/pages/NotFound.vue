2ee7257b0b4e935e6d0504b0834f8bddc3d4b03f
amin <aminarshadinia@gmail.com>
2024-10-04 03:31:31 +0800

    feat: create 404 page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>The page you are looking for doesn't exist.</p>
+    <router-link to="/">Go back to Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../scss/_variables.scss';
+
+.not-found {
+  text-align: center;
+  margin-top: 50px;
+}
+
+h1 {
+  font-size: 3em;
+  color: $primary-color;
+}
+
+p {
+  font-size: 1.2em;
+  color: $text-color;
+  font-weight: bold;
+}
+
+a {
+  font-size: 1.1em;
+  color: #3498db;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
