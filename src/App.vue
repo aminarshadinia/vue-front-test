b320450a9dc01ee73e285c10c60c2075798f9337
amin <aminarshadinia@gmail.com>
2024-10-04 00:40:08 +0800

    faet: add body style to app component

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,19 @@ export default {
 
 <style lang="scss">
 @import 'scss/main.scss';
+
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+  font-family: Arial, sans-serif;
+  background-color: #FAFAFA;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+
 </style>
