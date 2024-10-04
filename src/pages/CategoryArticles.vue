67e2b53ed16e3ef4e8ce87cec43ef78df2530989
amin <aminarshadinia@gmail.com>
2024-10-04 13:30:40 +0800

    chore: category article page cleanup

--- src/pages/CategoryArticles.vue
+++ src/pages/CategoryArticles.vue
@@ -1,53 +1,53 @@
 <template>
-<div class="wrapper">
+  <div class="wrapper">
     <SearchContainer @search="handleSearch" />
 
     <div v-if="loading" class="loading">Loading categories...</div>
     <div v-else-if="error" class="error">{{ error }}</div>
 
-  <div class="category-articles">
-    <div class="breadcrumb">
-      <router-link :to="{ name: 'Home' }" class="breadcrumb-link">All categories</router-link>
-      <span class="breadcrumb-separator">&gt;</span>
-      <span class="breadcrumb-current">{{ this.title }}</span>
-    </div>
+    <div class="category-articles">
+      <div class="breadcrumb">
+        <router-link :to="{ name: 'Home' }" class="breadcrumb-link">All categories</router-link>
+        <span class="breadcrumb-separator">&gt;</span>
+        <span class="breadcrumb-current">{{ title }}</span>
+      </div>
 
-    <div class="content">
-      <div class="sidebar">
-        <div class="card">
-          <div class="svg-button">
-            <icon :name="this.svgIcon"></icon>
-          </div>
-            <p class="title">{{ this.title }}</p>
-            <p class="subtitle">Updated {{ this.catUpdatedOn }}</p>
+      <div class="content">
+        <div class="sidebar">
+          <div class="card">
+            <div class="svg-button">
+              <icon :name="svgIcon"></icon>
+            </div>
+            <p class="title">{{ title }}</p>
+            <p class="subtitle">Updated {{ catUpdatedOn }}</p>
             <div class="border"></div>
 
-    <div class="info-icon">
-      <InfoIcon />
-    </div>
-    <p class="description">
-      De quibus cupio scire quid sentias. Quid, de quo nulla dissensio est Illum mallem levares sentias nulla dissensio.
-    </p>
-  
-  </div>
-      </div>
-      <div class="article-list">
-        <div v-for="article in publishedArticles" :key="article.id" class="article-item">
-          <div class="article-icon">
-            <ArticleIcon />
+            <div class="info-icon">
+              <InfoIcon />
+            </div>
+            <p class="description">
+              De quibus cupio scire quid sentias. Quid, de quo nulla dissensio est Illum mallem levares sentias nulla dissensio.
+            </p>
           </div>
-          <div class="article-content">
-            <h3>{{ article.title }}</h3>
-            <p>Updated {{ formattedDate(article.updatedOn) }}</p>
-          </div>
-          <div class="article-arrow">
-            <ArrowIcon />
+        </div>
+
+        <div class="article-list">
+          <div v-for="article in publishedArticles" :key="article.id" class="article-item">
+            <div class="article-icon">
+              <ArticleIcon />
+            </div>
+            <div class="article-content">
+              <h3>{{ article.title }}</h3>
+              <p>Updated {{ formattedDate(article.updatedOn) }}</p>
+            </div>
+            <div class="article-arrow">
+              <ArrowIcon />
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
