0af5c695dc2f33b78512969ffc53c6685161c847
amin <aminarshadinia@gmail.com>
2024-10-04 13:37:59 +0800

    feat: import and use search icon windget

--- src/components/layout/SearchContainer.vue
+++ src/components/layout/SearchContainer.vue
@@ -6,20 +6,28 @@
       <input type="text" v-model="localSearchQuery" placeholder="Search for answers">
       <button @click="performSearch">
         <span class="sr-only">Search</span>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
+        <SearchIcon />
       </button>
     </div>
   </div>
 </template>
 
 <script>
+import SearchIcon from '../widgets/SearchIcon.vue';
+
 export default {
   name: 'SearchContainer',
+
   data() {
     return {
       localSearchQuery: ''
     }
   },
+
+  components: {
+    SearchIcon,
+  },
+
   methods: {
     performSearch() {
       this.$emit('search', this.localSearchQuery)
