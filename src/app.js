2ed507d4f364ffc8223afce27cec9f65c3cd90fe
amin <aminarshadinia@gmail.com>
2024-10-04 22:20:10 +0800

    feat: create and register store to app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { router } from './router'
+import store from './store/index.js'
 
 
 Vue.use(VueRouter)
 
 new Vue({
 	router,
+	store,
 	render: h => h(App)
 }).$mount('#app')
