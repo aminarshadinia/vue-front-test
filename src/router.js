d5f435ad577443abb356658a369684783546758f
amin <aminarshadinia@gmail.com>
2024-10-10 17:04:56 +0800

    feat: add new search result route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home.vue";
 import CategoryArticles from "./pages/CategoryArticles.vue";
 import NotFound from "./pages/NotFound.vue";
 import VueRouter from 'vue-router'
+import SearchResult from "./pages/SearchResult.vue";
 
 export const routes = [
   {
@@ -19,6 +20,12 @@ export const routes = [
       svgIcon: route.params.svgIcon,
       catUpdatedOn: route.params.catUpdatedOn,
     })
+  },
+  {
+    path: '/search',
+    name: 'SearchResult',
+    component: SearchResult,
+    props: (route) => ({ localSearchQuery: route.query.q }) // Pass query param as a prop
   },
   {
     path: '/:pathMatch(.*)*',
